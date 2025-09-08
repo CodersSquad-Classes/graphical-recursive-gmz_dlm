de05c8dcbe7e5b2937ac987659462d669615f443
andresgomez31 <gomez.moreno.andresaugusto.18@gmail.com>
2025-09-08 07:52:37 -0600

    Testing 'src/koch' function with n = 3 and a 1000px canvas

--- main.cpp
+++ main.cpp
@@ -2,18 +2,33 @@
 
 #include "src/koch.hpp"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#define OUTPUT "lines.txt"
 
 int main () {
 	Point p1 = {0, 0};
-	Point p2 = {100, 0};
+	Point p2 = {1000, 0};
 	Line l = {p1, p2};
 
-	std::vector<Line> result = koch(l, 10);
+	std::vector<Line> result = koch(l, 3);
+
+	std::stringstream ss;
 
 	for (const Line &line : result) {
-		std::cout << "(" << line.first.first << ", " << line.first.second << "), ";
-		std::cout << "(" << line.second.first << ", " << line.second.second << ")" << std::endl;
+		ss << "(" << line.first.first << ", " << line.first.second << ")";
+		ss << "(" << line.second.first << ", " << line.second.second << ")" << std::endl;
 	}
+
+	std::ofstream file(OUTPUT);
+
+	if (!file) {
+		std::cerr << "Error writing on " << OUTPUT << "." << std::endl;
+		return -1;
+	}
+
+	file << ss.str();
 
 	return 0;
 }
