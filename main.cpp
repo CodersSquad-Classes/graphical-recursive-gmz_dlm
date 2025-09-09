86dbb796bf451a2fa037a11df72ef3ec16a10564
andresgomez31 <gomez.moreno.andresaugusto.18@gmail.com>
2025-09-08 21:16:58 -0600

    Accepts arguments from the user

--- main.cpp
+++ main.cpp
@@ -4,15 +4,34 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <stdexcept>
+
 
 #define OUTPUT "lines.txt"
 
-int main () {
+int main (int argc, char *argv[]) {
+	int n;
+	if (argc == 1) {
+		n = 4;
+	}
+
+	else if (argc > 2) {
+		throw std::invalid_argument("Invalid argument: Cannot enter more than 1 argument. Call it as follows: \n\t./main 4\nor\n\t./main 10");
+	}
+
+	try {
+		n = std::stoi(argv[1]);
+	}
+	catch (std::invalid_argument err) {
+		throw std::invalid_argument("Invalid argument: Cannot enter more than 1 argument. Call it as follows: \n\t./main 4\nor\n\t./main 10");
+	}
+
 	Point p1 = {0, 0};
 	Point p2 = {1000, 0};
 	Line l = {p1, p2};
 
-	std::vector<Line> result = koch(l, 3);
+	std::vector<Line> result = koch(l, n);
 
 	std::stringstream ss;
 
